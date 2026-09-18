d97e2ac83082f0c6deb8d0d98d5c548cfc43bf96
Código Café <contato@codigo.cafe>
2026-09-18 14:22:29 -0300

    feat: apply "about me" on the home page and create variables colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ const PageHome = () => {
             <section className={Home.container} >
                 <div className={Home.wrapper}>
                     <h1 className={Home.title}>Comece pelo básico, pelo rabisco, pelo esboço.<br/>É o alicerce da sua arte.</h1>
-                    {/* icone para ver mais */}
+                    
+                    <article className={Home.content}>
+                        <h2>Sobre a artista</h2>
+                        <p>Mariana Apapico é ilustradora e contadora de histórias, apaixonada pelo universo da ilustração infantil e das histórias para crianças. Em seu trabalho, une arte, imaginação e narrativa para criar imagens que despertam a curiosidade e dão vida a novos mundos.</p>
+                        <Link href="https://www.instagram.com/mariana_apapico/" target="_blank">Acompanhe o trabalho da Mariana no Instagram</Link>
+                    </article>
+
                     <div className={Home.frames}>
                         <Frames src="/images/art-000.webp" alt="E se A Viagem de Chihiro se passasse no Brasil?" />
                         <Frames src="/images/art-001.webp" alt="Eu personagem" />
